724e6b069cc3f563ae041f005af1ccdf92fcd73b
aakash21214 <93417145+aakash21214@users.noreply.github.com>
2024-03-11 11:03:26 +0000

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+#-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
+import os
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("6212279244:AAH7kw4PQhgGyuQPE7h3d0_2pamy7J-R4cc")
+    APP_ID = int(os.environ.get("12916125"))
+    API_HASH = os.environ.get("dfebf9cc52b859771cf8a1d447e751a5")
+    ADMIN = int(os.environ.get("1651746145"))
